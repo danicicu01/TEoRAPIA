c697eb119c903d820f5210ecf487a521aa7610e1
Dani Cicu <dani.cicu96@yahoo.com>
2022-12-01 23:33:11 +0200

    Connecting to the db

---
+++ TEoRAPIAweb/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TEoRAPIAweb.Model;
+
+namespace TEoRAPIAweb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+
+    }
+}
